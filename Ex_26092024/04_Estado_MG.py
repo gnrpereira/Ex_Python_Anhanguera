6c644655938e8bd9b11a58ece31c87e57efc141c
faculdade <faculdade@pgvlabcampus.br>
2024-09-26 21:24:27 -0300

    Exercicios da aula 26/09

---
+++ Ex_26092024/04_Estado_MG.py
@@ -0,0 +1,17 @@
+import mysql.connector
+
+connection = mysql.connector.connect(
+    user='govalp68_aluno', password='Pitagoras@2024',
+    host='srv236.prodns.com.br', database=' govalp68_pitagoras'
+)
+
+sql = connection.cursor()
+
+sql.execute("SELECT nome, endereco FROM clientes WHERE estado = 'MG'")
+
+resultados = sql.fetchall()
+
+print(resultados)
+
+sql.close()
+connection.close()
